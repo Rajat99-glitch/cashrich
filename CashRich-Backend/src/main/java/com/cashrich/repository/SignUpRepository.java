12a96d0d4b99e38d7f3bdbe52ad17f6272b839bd
Rajat Gawre <62611665+Rajat99-glitch@users.noreply.github.com>
2023-12-25 14:38:01 +0530

    Update SignUpRepository.java

--- CashRich-Backend/src/main/java/com/cashrich/repository/SignUpRepository.java
+++ CashRich-Backend/src/main/java/com/cashrich/repository/SignUpRepository.java
@@ -2,9 +2,11 @@ package com.cashrich.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
 import com.cashrich.entity.SignUpDetails;
+import com.cashrich.entity.UserDetails;
 
 @Component
 public interface SignUpRepository extends JpaRepository<SignUpDetails, Long>{
@@ -16,5 +18,8 @@ public interface SignUpRepository extends JpaRepository<SignUpDetails, Long>{
 	Optional<SignUpDetails> findById(Long id);
 	
 	Optional<SignUpDetails> findByUsernameAndPassword(String username, String password);
+	
+	@Query(value = "SELECT password FROM user_details WHERE username = ?", nativeQuery = true)
+	String findPasswordByUsername(String username);
 
 }
