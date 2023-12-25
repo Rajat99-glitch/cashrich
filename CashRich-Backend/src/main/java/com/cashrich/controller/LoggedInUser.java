34eff617f4805f04c0a52cbcd7b2b23e4ed7b3d6
Rajat Gawre <62611665+Rajat99-glitch@users.noreply.github.com>
2023-12-25 14:37:38 +0530

    Update LoggedInUser.java

--- CashRich-Backend/src/main/java/com/cashrich/controller/LoggedInUser.java
+++ CashRich-Backend/src/main/java/com/cashrich/controller/LoggedInUser.java
@@ -6,6 +6,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,8 +26,20 @@ public class LoggedInUser {
 
 	@GetMapping("/login")
 	public ResponseEntity<?> loginUser(@RequestBody UserDetails userDetails) {
-		
-		return null;
+		boolean match = false;
+		try {
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			String username = userDetails.getUsername();
+			String password = userDetails.getPassword();
+			String encryptedPassword = this.userRepo.findPasswordByUsername(username);
+			if (!encryptedPassword.isEmpty()) {
+				match = passwordEncoder.matches(password, encryptedPassword);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return match ? ResponseEntity.ok().body("Login Successful.....") : ResponseEntity.badRequest().body("Login credentials Incorrect.....");
 	}
 
 	@PutMapping("/update-user")
@@ -34,31 +47,35 @@ public class LoggedInUser {
 	public ResponseEntity<?> updateUser(@RequestBody SignUpDetails userDetails, @RequestParam Long userId)
 			throws UserNotFoundException {
 		
-		Optional<SignUpDetails> existingUser = userRepo.findById(userId);
-		if (existingUser.isEmpty()) {
-			ResponseEntity.badRequest().body("User not found with ID: " + userId);
-			throw new UserNotFoundException("User not found with ID: " + userId);
+		try {
+			Optional<SignUpDetails> existingUser = userRepo.findById(userId);
+			if (existingUser.isEmpty()) {
+				ResponseEntity.badRequest().body("User not found with ID: " + userId);
+				throw new UserNotFoundException("User not found with ID: " + userId);
+			}
+			if (userDetails.getFirstName() != null) {
+				existingUser.get().setFirstName(userDetails.getFirstName());
+			}
+			if (userDetails.getLastName() != null) {
+				existingUser.get().setLastName(userDetails.getLastName());
+			}
+			if (userDetails.getMobileNumber() != null) {
+				existingUser.get().setMobileNumber(userDetails.getMobileNumber());
+			}
+			if (userDetails.getUsername() != null) {
+				existingUser.get().setUsername(userDetails.getUsername());
+			}
+			if (userDetails.getEmail() != null) {
+				existingUser.get().setEmail(userDetails.getEmail());
+			}
+			if (userDetails.getPassword() != null) {
+				existingUser.get().setFirstName(userDetails.getPassword());
+			}
+			this.userRepo.save(existingUser.get());
+			return ResponseEntity.ok().body("User Updated Successfully......");
+		} catch (UserNotFoundException e) {
+			return ResponseEntity.badRequest().body(e);
 		}
-		if (userDetails.getFirstName() != null) {
-			existingUser.get().setFirstName(userDetails.getFirstName());
-		}
-		if (userDetails.getLastName() != null) {
-			existingUser.get().setLastName(userDetails.getLastName());
-		}
-		if (userDetails.getMobileNumber() != null) {
-			existingUser.get().setMobileNumber(userDetails.getMobileNumber());
-		}
-		if (userDetails.getUsername() != null) {
-			existingUser.get().setUsername(userDetails.getUsername());
-		}
-		if (userDetails.getEmail() != null) {
-			existingUser.get().setEmail(userDetails.getEmail());
-		}
-		if (userDetails.getPassword() != null) {
-			existingUser.get().setFirstName(userDetails.getPassword());
-		}
-		this.userRepo.save(existingUser.get());
-		return ResponseEntity.ok().body("User Updated Successfully......");
 	}
 
 }
