423fb582ae79818b0bae7c8b9b88ac5fc92769a0
Rajat Gawre <62611665+Rajat99-glitch@users.noreply.github.com>
2023-12-25 13:34:27 +0530

    Delete CashRich-Backend directory
    Some Files got misplaced here

--- CashRich-Backend/src/main/java/com/cashrich/repository/ThirdPartyApiRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cashrich.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cashrich.entity.ApiLogsEntity;
-
-public interface ThirdPartyApiRepo extends JpaRepository<ApiLogsEntity, Long>{
-
-}
--- CashRich-Backend/src/main/java/com/cashrich/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cashrich.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cashrich.entity.UserDetails;
-
-public interface UserRepository extends JpaRepository<UserDetails, Long>{
-
-}
